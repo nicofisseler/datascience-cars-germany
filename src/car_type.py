84cecb3acbfd73208e563b827b27316d311cce0c
nicof <nicofisseler@gmail.com>
2022-11-16 15:49:18 +0100

    added pie-chart

--- src/car_type.py
+++ src/car_type.py
@@ -3,4 +3,13 @@ import matplotlib.pyplot as plt
 
 car_type_dict = counter(5, lambda x: x[1])
 
-value, label = car_type_dict.values(), car_type_dict.keys()
+values, label = list(car_type_dict.values()), list(car_type_dict.keys())
+
+overall = sum(values)
+
+relative_values = [x/overall for x in values]
+
+label = [f'{label[i]} - {round(relative_values[i]*100)}%' for i in range(len(label))]
+
+plt.pie(relative_values, labels=label)
+plt.show()
