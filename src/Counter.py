4aa1c7cbd0827871f8ff34eb9b4d909db3507a72
nicof <nicofisseler@gmail.com>
2022-11-16 14:50:05 +0100

    restructured

---
+++ src/Counter.py
@@ -0,0 +1,17 @@
+import csv
+
+
+def counter(index, sorter, reversed=True):
+    with open('archive/postal.csv') as file:
+        dictionary = {}
+        csv_reader = csv.reader(file)
+        for val in list(csv_reader)[1:]:
+            value = val[index]
+            if len(value) > 1:
+                if value in dictionary:
+                    dictionary[value] += 1
+                else:
+                    dictionary[value] = 1
+        dictionary = dict(sorted(dictionary.items(), key=sorter, reverse=reversed))
+        return dictionary
+
