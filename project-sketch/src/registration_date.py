01c548f127e96abfcaf687b0abd515041f5f510a
nicof <nicofisseler@gmail.com>
2022-11-17 11:47:51 +0100

    adjust size

--- project-sketch/src/registration_date.py
+++ project-sketch/src/registration_date.py
@@ -1,11 +1,18 @@
+import matplotlib
+
 from lib.Counter import counter
 import matplotlib.pyplot as plt
 
-registration_date_dict = counter(6, lambda x: x[0], reversesort=False)
+registration_date_dict = counter(6, lambda x: x[0], is_reversed=False)
 
-values, labels = registration_date_dict.values(), registration_date_dict.keys()
+values, labels = registration_date_dict.values(), list(registration_date_dict.keys())
 
-figure, axis = plt.subplots()
-axis.tick_params(axis='x', labelsize=5, rotation=90)
+figure, axis = plt.subplots(figsize=(16, 8))
+axis.tick_params(axis='x', labelsize=12, rotation=90)
 plt.plot(labels, values)
+for i, label in enumerate(axis.xaxis.get_ticklabels()):
+    if i % 5 == 0 or i == len(labels) - 1:
+        label.set_visible(True)
+    else:
+        label.set_visible(False)
 plt.show()
