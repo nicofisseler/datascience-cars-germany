6789f27e97af7df5b04fb3c66e8a9b43870d80ba
Nico Fisseler <nicofisseler@gmail.com>
2022-11-29 09:19:30 +0100

    renamed, cleanup code, update map zoom

--- project-main/src/hetmap_and_barplot.py
+++ project-main/src/hetmap_and_barplot.py
@@ -1,14 +1,13 @@
 import csv
 import folium
+import pgeocode
 import matplotlib.pyplot as plt
 
 from folium.plugins import HeatMap
 
-# load data from ../archive/postal.csv
+# load data from ./archive/postal.csv
 with open('./archive/postal.csv') as csv_handler:
     csv_reader = list(csv.reader(csv_handler))[1:]
-
-    import pgeocode
 
     nomi = pgeocode.Nominatim('de')
 
@@ -28,10 +27,8 @@ with open('./archive/postal.csv') as csv_handler:
     # remove elements with missing coordinates
     postal_codes_coordinates = postal_codes_coordinates[postal_codes_coordinates.latitude.notna()]
 
-    # plot coordinates on an interactrive map using folium as heatmap
-
     # create map
-    data_map = folium.Map(location=[52.520008, 13.404954], zoom_start=7)
+    data_map = folium.Map(location=[52.520008, 13.404954], zoom_start=6)
     # create heatmap
     HeatMap(data=postal_codes_coordinates[['latitude', 'longitude']].values.tolist(), radius=12).add_to(data_map)
     # save map
@@ -41,7 +38,7 @@ with open('./archive/postal.csv') as csv_handler:
     postal_codes_by_district = {k: v for k, v in
                                 sorted(postal_codes_by_district.items(), key=lambda item: len(item[1]), reverse=True)}
 
-    # set figure size to 16 x 9
+    # set figure size to 24 x 16
     plt.figure(figsize=(24, 16))
     # plot bar chart with the first 10 elements
     plt.barh(list(postal_codes_by_district.keys())[:10][::-1], [len(x) for x in list(postal_codes_by_district.values())[:10]][::-1])
@@ -49,6 +46,6 @@ with open('./archive/postal.csv') as csv_handler:
     plt.tick_params(labelsize=28)
     # increase left space
     plt.subplots_adjust(left=0.3)
-    # set title to "Number of postal codes per district"
+    # set title
     plt.title("10 Bundesländer in denen am meisten Autos verkauft wurden", fontsize=40, fontweight="bold")
     plt.show()
